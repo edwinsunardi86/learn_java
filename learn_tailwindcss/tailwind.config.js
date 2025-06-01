1866b7130bb4336fc4e6f35556c72fd7916e7bc7
edwin <edwinsunardi86@gmail.com>
2025-06-01 22:19:04 +0700

    tailwindcss

---
+++ learn_tailwindcss/tailwind.config.js
@@ -0,0 +1,35 @@
+// tailwind.config.js
+module.exports = {
+    content:["./src/**/*.html", './src/**/*.{js,ts,jsx,tsx}'],
+    safelist: ["ps-[var(--sidebar-padding)]","pe-[var(--sidebar-padding)]"],
+    theme: {
+        extend: {
+            breakBefore: {
+                page: 'page',
+            },
+            breakAfter: {
+                page: 'page',
+            },
+            margin: {
+                custom: 'var(--my-margin)'
+            },
+        },
+    },
+    variants: {
+        extend: {
+        breakBefore: ['print'],
+        breakAfter: ['print'],
+        utilities: {
+            '.break-inside-avoid':{
+                breakInside: 'avoid',
+                pageBreakInside: 'avoid',
+                display: 'block'
+            }
+        }
+        },
+    },
+    plugins:[],
+    corePlugins: {
+        preflight: true
+    }
+}
